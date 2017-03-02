b0d66d3fedc0ea6a3a275bd7b3d0c303af9955f1
Dominik Masur <dominik.masur@gmail.com>
2017-03-02 06:59:58 +0100

    Better errors

--- src/app/services/appointments.service.ts
+++ src/app/services/appointments.service.ts
@@ -11,6 +11,7 @@ export class AppointmentsService {
   }
 
   getAllCalendars() {
+    if(gapi.client == undefined) return;
     return new Promise((resolve, reject) => {
       gapi.client.load('calendar', 'v3', function() {
         var request2 = gapi.client.calendar.calendarList.list({});
@@ -52,12 +53,16 @@ export class AppointmentsService {
         }
         resolve(appointments);
       });
+    }).catch((reason: any)=>{
+      console.log("getEvents Error: "+reason);
     });
   };
 
   loadAppointments() {
     return new Promise((resolve, reject) => {
-      this.getAllCalendars().then(calendars => {
+      var calendars = this.getAllCalendars()
+      if(calendars == undefined) return;
+      calendars.then(calendars => {
         var eventCalls = [];
 
         for (var i = 0; i < (calendars as Array<ICalendarListEntry>).length; i++) {
@@ -67,6 +72,8 @@ export class AppointmentsService {
           resolve([].concat.apply([], result));
         });
       });
+    }).catch((reason: any)=>{
+      console.log("loadApppointment Error: "+reason);
     });
   }
 }
